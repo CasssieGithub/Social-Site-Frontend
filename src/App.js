0a5548c8cd5517c2f4bbfe70c35c21b53a9c8ab2
momot <motraore611@yahoo.com>
2023-03-31 12:40:09 -0400

    testing for show

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import "./App.css";
+import {useState, useEffect} from 'react'
 import { Routes, Route } from "react-router-dom";
+import axios from 'axios'
 import Home from "./components/Home";
 import Add from "./components/Add";
 import View from "./components/View";
 
 const App = () => {
+  const [posts, setPosts] = useState([])
+
+  const getPosts = () => {
+    axios.get('http://localhost:3000/posts').then((response) => setPosts(response.data), (err) => console.log(err))
+  }
+
+  const handleCreate = (data) => {
+    axios.post('http://localhost:3000/posts', data).then((response) => {
+      console.log(response);
+      let newPosts = [...posts, response.data]
+      setPosts(newPosts)
+    })
+  }
+
+  useEffect(() => {
+    getPosts()
+  }, [])
+
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home handleCreate={handleCreate}/>} />
         <Route path="add" element={<Add />} />
         <Route path="viewposts" element={<View />} />
       </Routes>
--- src/components/View.js
+++ src/components/View.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const View = () => {
+const View = (props) => {
   return (
     <div>
       <h1>View posts here</h1>
+
+      <div>
+        <h3>Posts: {props.posts.name}</h3>
+      </div>
+
       <Link to="/">Home</Link>
     </div>
   );
