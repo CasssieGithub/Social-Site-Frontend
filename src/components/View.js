1fe6496d3d1a42ae2d55c609d26bebf7f01ac034
Cassie Hernandez <cassie@Cassies-MacBook-Air.local>
2023-03-30 17:58:55 -0700

    updated home and user

--- src/components/View.js
+++ src/components/View.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const View = () => {
   return (
     <div>
-      <h1>View posts here</h1>
+      <h1>View pofile here</h1>
       <Link to="/">Home</Link>
     </div>
   );
