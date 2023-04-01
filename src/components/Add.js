be83de7c832733cfee2126240cb1ca9060168913
momot <motraore611@yahoo.com>
2023-03-31 21:38:50 -0400

    still attempting to pull from dev

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,41 +1,6 @@
-<<<<<<< HEAD
 import React from "react";
-<<<<<<< HEAD
 import {useState} from 'react'
 import { Link } from "react-router-dom";
-
-const Add = (props) => {
-  const [posts, setPosts] = useState({text: '', image: '', link: ''})
-
-  const handleChange = (event) => {
-    setPosts({...posts, [event.target.name]: event.target.value})
-  }
-
-  const handleSubmit = (event) => {
-    event.preventDefault()
-    props.handleCreate(posts)
-  }
-
-  return (
-    <div>
-      <h1>Create a form here</h1>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor='name'>Name: </label>
-        <input type='text' name='name' onChange={handleChange}></input>
-        <br/>
-        <label htmlFor='name'>Image: </label>
-        <input type='text' name='image' onChange={handleChange}></input>
-        <br/>
-        <label htmlFor='name'>Link: </label>
-        <input type='text' name='link' onChange={handleChange}></input>
-        <br/>
-        <input type='submit'></input>
-      </form>
-      <Link to="/">Home</Link>
-    </div>
-=======
-// import { Link } from "react-router-dom";
-import { useState } from "react";
 import "./Add.css";
 
 const Add = (props) => {
@@ -83,11 +48,5 @@ const Add = (props) => {
         </div>
       </>
     </>
->>>>>>> ed95e19002e8ba9fed0da2975ee640667281f8f5
-  );
-};
-
-export default Add;
-=======
-import React from "react";
->>>>>>> d6812082cf377918a91bea43d9a04df94fc152bf
+  )
+  }
